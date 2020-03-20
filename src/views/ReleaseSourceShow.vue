a491b9ab7b05838188c25700b0f327daddd3cf9e
madirysbek <hooizit@gmail.com>
2020-03-20 07:12:15 +0600

    fix: release source show

--- src/views/ReleaseSourceShow.vue
+++ src/views/ReleaseSourceShow.vue
@@ -4,7 +4,7 @@
     <div class="page-header row no-gutters py-4">
       <div class="col-12 col-sm-4 text-center text-sm-left mb-0">
         <span class="text-uppercase page-subtitle">Источник выделения </span>
-        <h3 class="page-title">{{ releaseSourceCard.releaseSource.releaseSourceName }}</h3>
+        <h3 class="page-title">{{ releaseSourceCard.releaseSourceName }}</h3>
       </div>
     </div>
 
@@ -22,51 +22,51 @@
           >
             <tr>
               <th>Наименование промплощадки:</th>
-              <td class="font-weight-normal">{{ releaseSourceCard.worksite.workSiteName }}</td>
+              <td class="font-weight-normal">{{ releaseSourceCard.workSiteName }}</td>
             </tr>
             <tr>
               <th>Категория промплощадки:</th>
-              <td class="font-weight-normal">{{ releaseSourceCard.worksite.natureUserCategory }}</td>
+              <td class="font-weight-normal">{{ releaseSourceCard.natureUserCategory }}</td>
             </tr>
             <tr>
               <th>Цех / объект:</th>
-              <td class="font-weight-normal">{{ releaseSourceCard.facility.facilityName }}</td>
+              <td class="font-weight-normal">{{ releaseSourceCard.facilityName }}</td>
             </tr>
             <tr>
               <th>Участок:</th>
-              <td class="font-weight-normal">{{ releaseSourceCard.facilityLocation.facilityLocationName }}</td>
+              <td class="font-weight-normal">{{ releaseSourceCard.facilityLocationName }}</td>
             </tr>
             <tr>
               <th>№ ист.эмиссии:</th>
-              <td class="font-weight-normal">{{ releaseSourceCard.emissionSource.emissionSourceNumber }}</td>
+              <td class="font-weight-normal">{{ releaseSourceCard.emissionSourceNumber }}</td>
             </tr>
             <tr>
               <th>Наименование ист. выбросов:</th>
-              <td class="font-weight-normal">{{ releaseSourceCard.emissionSource.emissionSourceName }}</td>
+              <td class="font-weight-normal">{{ releaseSourceCard.emissionSourceName }}</td>
             </tr>
             <tr>
               <th>Тип ист.эмиссии:</th>
-              <td class="font-weight-normal">{{ releaseSourceCard.emissionSource.emissionSourceOrganized ? 'организованный' : 'неорганизованный' }}</td>
+              <td class="font-weight-normal">{{ releaseSourceCard.emissionSourceOrganized ? 'организованный' : 'неорганизованный' }}</td>
             </tr>
             <tr>
               <th>Наименование ист.выделения:</th>
-              <td class="font-weight-normal">{{ releaseSourceCard.releaseSource.releaseSourceName }}</td>
+              <td class="font-weight-normal">{{ releaseSourceCard.releaseSourceName }}</td>
             </tr>
             <tr>
               <th>Номер ист.выделения:</th>
-              <td class="font-weight-normal">{{ releaseSourceCard.releaseSource.releaseSourceNumber }}</td>
+              <td class="font-weight-normal">{{ releaseSourceCard.releaseSourceNumber }}</td>
             </tr>
-            <tr>
+            <!-- <tr>
               <th>Описание источника:</th>
               <td class="font-weight-normal">{{ releaseSourceCard.emissionSource.description }}</td>
-            </tr>
+            </tr> -->
           </table>
         </d-card-body>
       </div>
       <d-card-footer class="text-right border-top">
         <div class="d-flex">
-          <d-link @click="goBack">Назад</d-link>
-          <router-link class="ml-auto"  :to="{ name: 'release-source-edit', params: { id: releaseSourceCard.releaseSource.id }}">
+          <router-link to="/release-source-list">Назад</router-link>
+          <router-link class="ml-auto"  :to="{ name: 'release-source-edit', params: { id: releaseSourceCard.id }}">
             <d-button theme="warning">Редактировать</d-button>
           </router-link>
         </div>
@@ -76,32 +76,41 @@
 </template>
 
 <script>
-import { mapState, mapActions } from 'vuex';
+import api from '@/services/api';
 
 export default {
   data() {
     return {
       loaded: false,
+      releaseSourceCard: {},
     };
   },
   created() {
     this.getSource();
   },
   computed: {
-    ...mapState('emissions', ['releaseSourceCard']),
   },
   methods: {
-    ...mapActions('emissions', ['getReleaseSourceCard', 'resetReleaseSourceCard']),
-    getSource() {
-      try {
-        this.loaded = false;
-        this.getReleaseSourceCard(this.$route.params.id);
-      } finally {
-        this.loaded = true;
-      }
-    },
-    goBack() {
-      this.$router.go(-1);
+    async getSource() {
+      this.loaded = false;
+      const releaseSource = await api.getResource(`releaseSources/${this.$route.params.id}`);
+      const worksite = await api.getResource(`worksites/${releaseSource.worksiteId}`);
+      const facility = await api.getResource(`facilities/${releaseSource.facilityId}`);
+      const facilityLocation = await api.getResource(`facilityLocations/${releaseSource.facilityLocationId}`);
+      const emissionSource = await api.getResource(`emissionSources/${releaseSource.emissionSourceId}`);
+      this.releaseSourceCard = {
+        id: releaseSource.id,
+        workSiteName: worksite.workSiteName,
+        natureUserCategory: worksite.natureUserCategory,
+        facilityName: facility.facilityName,
+        facilityLocationName: facilityLocation.facilityLocationName,
+        emissionSourceNumber: emissionSource.emissionSourceNumber,
+        emissionSourceName: emissionSource.emissionSourceName,
+        emissionSourceOrganized: emissionSource.emissionSourceOrganized,
+        releaseSourceName: releaseSource.releaseSourceName,
+        releaseSourceNumber: releaseSource.releaseSourceNumber,
+      };
+      this.loaded = true;
     },
   },
 };
