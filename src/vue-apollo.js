1f4dc9ce89186b93384a1b57129d316626539101
Madi Rysbek <hooizit@gmail.com>
2021-07-09 02:24:21 +0600

    fix: auth middleware

--- src/vue-apollo.js
+++ src/vue-apollo.js
@@ -7,17 +7,16 @@ import { HttpLink } from 'apollo-link-http'
 import { InMemoryCache } from 'apollo-cache-inmemory'
 import { ApolloLink } from 'apollo-link'
 
-const authToken = localStorage.getItem('token') || null
-
 const httpLink = new HttpLink({
   uri: process.env.VUE_APP_GRAPHQL_HTTP || 'https://hasura.ecomarine.kz/v1/graphql'
 })
 
 const authMiddleware = new ApolloLink((operation, forward) => {
+  const token = localStorage.getItem("token")
   operation.setContext(( { headers = {} }) => ({
-    headers: authToken ? {
+    headers: token ? {
       ...headers,
-      authorization: `Bearer ${authToken}`
+      authorization: `Bearer ${token}`
     } :
     {
       ...headers,
