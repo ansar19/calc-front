5bb641a0df1e084227ab3255e8b6b17143c07756
Madi Rysbek <hooizit@gmail.com>
2021-09-15 20:07:34 +0600

    fix: list-companies

--- src/views/ListCompanies.vue
+++ src/views/ListCompanies.vue
@@ -1,50 +1,41 @@
 <script>
-import Vue from "vue";
-import { ClientTable } from "vue-tables-2";
-import "@/assets/scss/vue-tables.scss";
+import Spinner from "@/components/Base/Spinner.vue";
 import COMPANIES from "../graphql/queries/CompanyList";
-import { useGlobalState } from '@/useStore'
-
-Vue.use(ClientTable);
+import { useGlobalState } from "@/useStore";
+import { useQuery, useResult } from "@vue/apollo-composable";
 
 export default {
-  setup() {
-    const state = useGlobalState()
-    function setCompany(id, name) {
-      state.value.companyId = id
-      state.value.companyName = name
-    }
-    return { setCompany }
-  },
+  setup(_, ctx) {
+    const router = ctx.root.$router;
 
-  apollo: {
-    companies: {
-      query:  COMPANIES
+    const state = useGlobalState();
+    const { result, loading, error } = useQuery(COMPANIES);
+    const companies = useResult(result, null, (data) => data.companies);
+    function setCompany(props) {
+      state.value.companyId = props.id;
+      state.value.companyName = props.company_name;
     }
-  },
+    function viewCompany({ id }) {
+      router.push({ name: "show-company", params: { id } });
+    }
 
-  components: {
-    ClientTable
+    function editCompany({ id }) {
+      router.push({ name: "edit-company", params: { id } });
+    }
+    return { viewCompany, setCompany, editCompany, companies, loading, error };
   },
+  components: { Spinner },
+
   data() {
     return {
-      // fields: FieldsDef(this.$i18n),
       componentTitle: "Список компаний",
-      companies: [],
-      pageerrors: [],
-      locale: "ru",
-      dateRange: {
-        from: null,
-        to: null
-      },
-
       columns: [
-        "company_name",
-        "iin_bin",
-        "company_type.short_name",
-        "company_email",
-        "company_phone",
-        "actions"
+        { label: "Название компании", field: "company_name" },
+        { label: "БИН / ИНН", field: "iin_bin" },
+        { label: "Тип юр лица", field: "company_type.short_name" },
+        { label: "Email компании", field: "company_email" },
+        { label: "Телефон компании", field: "company_phone" },
+        { label: "", field: "action" },
       ],
 
       // this part related to excel export
@@ -52,7 +43,7 @@ export default {
         "Название компании": "companyName",
         "Тип юридического лица": {
           field: "companyType.label",
-          callback: value => `Тип: ${value}`
+          callback: (value) => `Тип: ${value}`,
         },
         "БИН / ИНН": "binInn",
         "Телефон компании": "companyPhone",
@@ -70,64 +61,16 @@ export default {
           "companyAddress.actual.addressComments",
         "Страна юридический адрес": "companyAddress.legal.country",
         "Адрес юридический": "companyAddress.legal.address",
-        "Почтовый индекс юридический": "companyAddress.legal.zip"
+        "Почтовый индекс юридический": "companyAddress.legal.zip",
       },
       json_meta: [
         [
           {
             key: "charset",
-            value: "utf-8"
-          }
-        ]
+            value: "utf-8",
+          },
+        ],
       ],
-
-      companyTableOptions: {
-        headings: {
-          company_name: "Название компании",
-          iin_bin: "БИН / ИНН",
-          "company_type.short_name": "Тип юр лица",
-          company_email: "Email компании",
-          company_phone: "Телефон компании",
-          actions: "Действия"
-        },
-        perPage: 10,
-        recordsPerPage: [10, 25, 50, 100],
-        skin: "transaction-history table dataTable",
-        filterable: [
-          "company_name",
-          "iin_bin",
-          "company_type.short_name",
-          "company_email",
-          "company_phone"
-        ],
-        sortable: [
-          "company_name",
-          "iin_bin",
-          "company_type.short_name",
-          "company_email",
-          "company_phone"
-        ],
-        sortIcon: {
-          base: "fas float-right mt-1 text-muted",
-          up: "fa-caret-up",
-          down: "fa-caret-down"
-        },
-        texts: {
-          filterPlaceholder: "Введите текст для поиска",
-          limit: "Записей",
-          filter: "Поиск",
-          noResults: "Нет подходящих записей",
-          loading: "Идет загрузка...",
-          page: "Стр.:",
-          filterBy: "Фильтровать по {column}",
-          count:
-            "Отображение с {from} по {to} из {count} записей|{count} записей | Запись"
-        },
-        pagination: {
-          edge: true,
-          nav: "scroll"
-        }
-      }
     };
   },
 };
@@ -147,7 +90,11 @@ export default {
     <d-card class="card-small mb-3">
       <d-card-header class="border-bottom">
         <div class="block-handle" align="right">
-          <d-button size="sm" class="btn-info btn-sm ml-2" v-d-tooltip.hover="'Выгрузить Excel'">
+          <d-button
+            size="sm"
+            class="btn-info btn-sm ml-2"
+            v-d-tooltip.hover="'Выгрузить Excel'"
+          >
             <download-excel
               :data="companies"
               title="Список компании"
@@ -165,98 +112,56 @@ export default {
         </div>
       </d-card-header>
       <d-card-body>
-        <div class="meta-table">
-          <!-- List Manager -->
-          <v-client-table
-            class="dataTables_wrapper"
-            :data="companies"
+        <spinner v-if="loading" />
+        <div v-else-if="error">Ошибка: {{ error.message }}</div>
+        <template v-else-if="companies">
+          <vue-good-table
             :columns="columns"
-            :options="companyTableOptions"
+            :rows="companies"
+            :fixed-header="true"
+            compactMode
+            :pagination-options="{
+              enabled: true,
+              mode: 'pages',
+              perPageDropdown: [10, 50, 100],
+              rowsPerPageLabel: 'Строк',
+              nextLabel: 'Вперед',
+              prevLabel: 'Назад',
+              allLabel: 'Все',
+            }"
           >
-            <div slot="child_row" slot-scope="props">
-              <table class="meta-table" id="example-1">
-                <thead>
-                  <tr>
-                    <th style="width:25%">Адрес фактический</th>
-                    <th style="width:25%">Адрес юридический</th>
-                    <th style="width:25%">Банковские детали</th>
-                    <th style="width:25%">ФИО первого руководителя</th>
-                  </tr>
-                </thead>
-                <tr>
-                  <td data-label="Адрес фактический:">
-                    {{props.row.company_actual_address.city.state.country.name}}, {{props.row.company_actual_address.city.state.name}}, {{props.row.company_actual_address.city.name}}, {{props.row.company_actual_address.address}},
-                    {{props.row.company_actual_address.zip}}
-                  </td>
-                  <td data-label="Адрес юридический:">
-                    {{props.row.company_legal_address.city.state.country.name}}, {{props.row.company_legal_address.city.state.name}}, {{props.row.company_legal_address.city.name}}, {{props.row.company_legal_address.address}},
-                    {{props.row.company_legal_address.zip}}
-                  </td>
-                  <!-- <td data-label="Банковские детали:">
-                    <ul v-for="bankAccount in props.row.bank_accounts" :key="bankAccount.id">
-                      <li>
-                        {{bankAccount.iban}} {{bankAccount.currency.name}}
-                        {{bankAccount.bank.name}}
-                      </li>
-                    </ul>
-                  </td> -->
-                  <!-- <td data-label="Данные первого руководителя:">
-                    {{props.row.companyHead.companyHeadSurename}} {{props.row.companyHead.companyHeadFirstname}}
-                    {{props.row.companyHead.companyHeadPatronyme}}
-                    <br />
-                    {{props.row.companyHead.companyHeadPosition}}
-                    <br />
-                    {{props.row.companyHead.companyHeadPhone}}
-                    <br />
-                    {{props.row.companyHead.companyHeadEmail}}
-                  </td> -->
-                </tr>
-              </table>
-            </div>
-
-            <d-button-group
-              slot="actions"
-              slot-scope="props"
-              size="small"
-              class="d-flex justify-content-center"
-            >
-              <d-button
-                class="btn-white"
-                @click="setCompany(props.row.id, props.row.company_name)"
-                v-d-tooltip.hover="'Войти в компанию'"
-              >
-                <i class="material-icons">&#xE5CA;</i>
-              </d-button>
-              <router-link :to="{ name: 'show-company', params: { id: props.row.id }}">
-                <d-button class="btn-white" v-d-tooltip.hover="'Посмотреть'">
-                  <i class="material-icons">&#xe417;</i>
-                </d-button>
-              </router-link>
-
-              <router-link :to="{ name: 'edit-company', params: { id: props.row.id }}">
-                <d-button class="btn-white" v-d-tooltip.hover="'Редактировать'">
-                  <i class="material-icons">&#xE254;</i>
-                </d-button>
-              </router-link>
-
-              <d-button
-                class="btn-white"
-                @click="deleteCompany(props.row.id)"
-                v-d-tooltip.hover="'Удалить'"
-              >
-                <i class="material-icons">&#xE872;</i>
-              </d-button>
-            </d-button-group>
-
-            <span
-              slot="status"
-              slot-scope="props"
-              :class="[getStatusClass(props.row.status)]"
-            >{{ props.row.status }}</span>
-
-            <span slot="total" slot-scope="props" class="text-success">{{ props.row.total }}</span>
-          </v-client-table>
-        </div>
+            <template slot="table-row" slot-scope="props">
+              <span v-if="props.column.field == 'action'">
+                <div
+                  class="btn-group d-flex"
+                  role="group"
+                  aria-label="Comapny actions"
+                >
+                  <button
+                    type="button"
+                    class="btn btn-white"
+                    @click="setCompany(props.row)"
+                  >
+                    <i class="material-icons">&#xE5CA;</i> Войти
+                  </button>
+                  <button
+                    type="button"
+                    class="btn btn-white"
+                    @click="viewCompany(props.row)"
+                  >
+                    <i class="material-icons">&#xe417;</i> Открыть
+                  </button>
+                  <button type="button" class="btn btn-white" @click="editCompany(props.row)">
+                    <i class="material-icons">&#xE254;</i> Редактировать
+                  </button>
+                </div>
+              </span>
+              <span v-else>
+                {{ props.formattedRow[props.column.field] }}
+              </span>
+            </template>
+          </vue-good-table>
+        </template>
       </d-card-body>
     </d-card>
   </d-container>
