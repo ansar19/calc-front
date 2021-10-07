23d1b9461bb1de1f8fd60a46f21c785fbb7a869a
Madi Rysbek <hooizit@gmail.com>
2021-10-07 18:13:42 +0600

    fix: before

--- src/components/pollutant-edit/pollutant-edit.vue
+++ src/components/pollutant-edit/pollutant-edit.vue
@@ -1,7 +1,7 @@
 <template>
   <d-list-group flush>
     <d-list-group-item class="px-3">
-      <Spinner v-if="loading" />
+      <!-- <Spinner v-if="loading" />
       <div v-else-if="error">Ошибка: {{ error.message }}</div>
       <div v-else-if="pollutant && groups">
         <h6>Загрязняющее вещество</h6>
@@ -35,86 +35,86 @@
           @click="savePol"
         />
         <Spinner v-if="groupMutateLoading" />
-      </div>
+      </div> -->
     </d-list-group-item>
   </d-list-group>
 </template>
 
 <script>
-import POLL_BY_ID_AND_GROUPS from "@/graphql/queries/PollutantByIdAndGroupsList";
-import ADD_POL_TO_GROUP from "@/graphql/mutations/AddPolTogroup";
-import POLLS_LIST from "@/graphql/queries/PollutantsList";
+// import POLL_BY_ID_AND_GROUPS from "@/graphql/queries/PollutantByIdAndGroupsList";
+// import ADD_POL_TO_GROUP from "@/graphql/mutations/AddPolTogroup";
+// import POLLS_LIST from "@/graphql/queries/PollutantsList";
 
-import { useMutation, useQuery, useResult } from "@vue/apollo-composable";
-import { ref } from "@vue/composition-api";
+// import { useMutation, useQuery, useResult } from "@vue/apollo-composable";
+// import { ref } from "@vue/composition-api";
 
-export default {
-  props: {
-    pollId: {
-      type: String,
-      required: true,
-    },
-    slideToggle: {
-      type: Function,
-      required: true,
-    },
-  },
-  setup(props) {
-    const { pollId, slideToggle } = props;
-    const { result, loading, error } = useQuery(POLL_BY_ID_AND_GROUPS, {
-      id: pollId,
-    });
-    const groupId = ref("");
-    const validationMsg = ref("");
-    const pollutant = useResult(result, null, (data) => {
-      groupId.value = data.air_pollutants_by_pk.group
-        ? data.air_pollutants_by_pk.group.id
-        : "-";
-      return data.air_pollutants_by_pk;
-    });
-    const groups = useResult(result, null, (data) => data.air_pollutant_groups);
+// export default {
+//   props: {
+//     pollId: {
+//       type: String,
+//       required: true,
+//     },
+//     slideToggle: {
+//       type: Function,
+//       required: true,
+//     },
+//   },
+//   setup(props) {
+//     const { pollId, slideToggle } = props;
+//     const { result, loading, error } = useQuery(POLL_BY_ID_AND_GROUPS, {
+//       id: pollId,
+//     });
+//     const groupId = ref("");
+//     const validationMsg = ref("");
+//     const pollutant = useResult(result, null, (data) => {
+//       groupId.value = data.air_pollutants_by_pk.group
+//         ? data.air_pollutants_by_pk.group.id
+//         : "-";
+//       return data.air_pollutants_by_pk;
+//     });
+//     const groups = useResult(result, null, (data) => data.air_pollutant_groups);
 
-    const {
-      mutate: groupMutate,
-      loading: groupMutateLoading,
-      error: groupMutateError,
-      onDone,
-    } = useMutation(ADD_POL_TO_GROUP, () => ({
-      variables: {
-        pollutant_id: pollutant.value.id,
-        pollutant_group_id: groupId.value,
-      },
-      update: (cache, groupMutate) => {
-        const updatedPoll = groupMutate.data.insert_air_pollutants_groups;
-        cache.evict({
-          id: "ROOT_QUERY",
-          field: "air_pollutants",
-          args: { id: updatedPoll.id },
-        });
-      },
-    }));
+//     const {
+//       mutate: groupMutate,
+//       loading: groupMutateLoading,
+//       error: groupMutateError,
+//       onDone,
+//     } = useMutation(ADD_POL_TO_GROUP, () => ({
+//       variables: {
+//         pollutant_id: pollutant.value.id,
+//         pollutant_group_id: groupId.value,
+//       },
+//       update: (cache, groupMutate) => {
+//         const updatedPoll = groupMutate.data.insert_air_pollutants_groups;
+//         cache.evict({
+//           id: "ROOT_QUERY",
+//           field: "air_pollutants",
+//           args: { id: updatedPoll.id },
+//         });
+//       },
+//     }));
 
-    function savePol() {
-      if (groupId.value === "-") {
-        validationMsg.value = "Выберите группу!";
-        return;
-      }
-      groupMutate();
-    }
+//     function savePol() {
+//       if (groupId.value === "-") {
+//         validationMsg.value = "Выберите группу!";
+//         return;
+//       }
+//       groupMutate();
+//     }
 
-    onDone(() => slideToggle());
+//     onDone(() => slideToggle());
 
-    return {
-      pollutant,
-      groups,
-      loading,
-      error,
-      savePol,
-      groupId,
-      validationMsg,
-      groupMutateLoading,
-      groupMutateError,
-    };
-  },
-};
+//     return {
+//       pollutant,
+//       groups,
+//       loading,
+//       error,
+//       savePol,
+//       groupId,
+//       validationMsg,
+//       groupMutateLoading,
+//       groupMutateError,
+//     };
+//   },
+// };
 </script>
