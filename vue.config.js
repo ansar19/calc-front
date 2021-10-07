6d3747de78fd3e5f3a20f73c21de59d568319203
Madi Rysbek <hooizit@gmail.com>
2021-10-07 19:27:39 +0600

    fix: remove unused plugin

--- vue.config.js
+++ vue.config.js
@@ -1,16 +1,11 @@
 
 /* eslint-disable @typescript-eslint/no-var-requires */
 
-const scriptSetupOptions = {
-  refTransform: false,
-}
-
 /** @type {import('@vue/cli-service').ProjectOptions} */
 module.exports = {
     productionSourceMap: false,
     pluginOptions: {
       vite: {
-        plugins: [require('unplugin-vue2-script-setup/vite')(scriptSetupOptions)],
         disabledTypeChecker: true,
         disabledLint: true,
         optimizeDeps: {
